091f6b2f4cdf2242250aca22340202223e13a61b
Emanuele <emanuelesquillante@libero.it>
2024-09-30 17:56:47 +0200

    db

--- config/db.php
+++ config/db.php
@@ -1,15 +1,17 @@
 <?php
 // Configurazione del database
-$servername = "localhost";
-$username = "root"; // Nome utente di default per XAMPP
-$password = "";     // Password di default è vuota
-$dbname = "notesapp_db";
+$servername = "dpg-crtcf252ng1s73bvnngq-a"; // Hostname da Render
+$username = "dbsimplynotes_user";            // Username da Render
+$password = "relDbjxCEhkxDcUmUAfml4SDkD8HFbFm";        // Password da Render
+$dbname = "dbsimplynotes";                   // Nome del database da Render
+$port = 5432;                                // Porta predefinita di PostgreSQL
 
 // Creazione della connessione
-$conn = new mysqli($servername, $username, $password, $dbname);
+$conn = new mysqli($servername, $username, $password, $dbname, $port);
 
 // Controllo della connessione
 if ($conn->connect_error) {
     die("Connessione fallita: " . $conn->connect_error);
 }
 ?>
+
