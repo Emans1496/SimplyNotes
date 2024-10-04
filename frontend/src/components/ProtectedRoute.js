ed945d9c607df0d2e49407ca30959a63f771b335
Emanuele <emanuelesquillante@libero.it>
2024-10-04 09:41:20 +0200

    protected route remove

--- frontend/src/components/ProtectedRoute.js
+++ frontend/src/components/ProtectedRoute.js
@@ -13,14 +13,7 @@ function App() {
         <Routes>
           <Route path="/" element={<Login />} />
           <Route path="/register" element={<Register />} />
-          <Route
-            path="/dashboard"
-            element={
-              <ProtectedRoute>
-                <Dashboard />
-              </ProtectedRoute>
-            }
-          />
+          <Route path="/dashbiard" element={<Dashboard />} />
         </Routes>
       </div>
     </Router>
